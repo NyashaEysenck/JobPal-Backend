820e1f35bee6eb91e2bb8b165d47ca59a6001ca9
Eysenck Gandah <nyashafpl@gmail.com>
2025-03-27 18:07:06 +0200

    Updates

--- app.py
+++ app.py
@@ -340,7 +340,7 @@ def get_interview_questions():
     if request.method == 'OPTIONS':
         # Respond to preflight
         response = jsonify()
-        response.headers.add("Access-Control-Allow-Origin", "http://yourfrontend.com")
+        response.headers.add("Access-Control-Allow-Origin", "https://jobpal-frontend-production.up.railway.app")
         response.headers.add("Access-Control-Allow-Methods", "POST, OPTIONS")
         response.headers.add("Access-Control-Allow-Headers", "Content-Type, Authorization")
         response.headers.add("Access-Control-Allow-Credentials", "true")  # If using cookies
